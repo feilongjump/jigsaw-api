bc22d75d1a0bce4d5041e3b682e97b55047acaf7
ifeilong <feilongjump@qq.com>
2026-01-10 20:15:35 +0800

    添加跨域中间件支持

---
+++ api/middleware/cors.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Cors 跨域
+func Cors() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		method := ctx.Request.Method
+
+		if method != "" {
+			// 可将 * 替换为指定的域名
+			ctx.Header("Access-Control-Allow-Origin", "*")
+			ctx.Header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE")
+			ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			ctx.Header("Access-Control-Allow-Credentials", "true")
+			ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+		}
+
+		if method == "OPTIONS" {
+			ctx.Status(204)
+			return
+		}
+
+		ctx.Next()
+	}
+}
--- api/router/router.go
+++ api/router/router.go
@@ -1,13 +1,26 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/feilongjump/jigsaw-api/api/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 func Init() *gin.Engine {
 
 	r := gin.Default()
 
+	// 注册中间件
+	registerMiddlewares(r)
+
 	RegisterNoteRouter(r)
 	RegisterUserRouter(r)
 
 	return r
+}
+
+// 注册中间件
+func registerMiddlewares(r *gin.Engine) {
+	r.Use(
+		middleware.Cors(),
+	)
 }
